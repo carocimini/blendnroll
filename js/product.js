9de79fceabe0a7856ce3ade5f140e8b34b32bfcc
Carola Cimini <caro.cimini@gmail.com>
2021-11-02 16:12:10 -0300

    aplicando objetos/metodos y clases en javascript

--- js/product.js
+++ js/product.js
@@ -1,24 +1,38 @@
-let precio = 450;
-let cantidadIngresada = 0;
-let precioFinal = 0;
+class Producto{
+    constructor (nombre, rubro, precio, peso, tamaño, marca, sabor, stock, cantidadComprar, precioVenta){
+        this.nombre = nombre;
+        this.rubro = rubro;
+        this.precio = precio;
+        this.peso = peso;
+        this.tamaño = tamaño;
+        this.marca = marca;
+        this.sabor = sabor;
+        this.stock = stock;
+        this.cantidadComprar = cantidadComprar;
+        this.precioVenta = precioVenta;
+    }
+    iva(){
+        this.precio = this.precio * 1.21;
+    }
+    venta(){
+        this.cantidadComprar = prompt("Ingresar cantidad deseada: ");
+        alert("La cantidad que deseas comprar es " + this.cantidadComprar);
+        this.precioVenta = this.cantidadComprar * this.precio
+        alert("El valor final de tu compra es: $" + this.precioVenta);
+        this.stock = this.stock - this.cantidadComprar;
+    }
+    resetVentaFinalizada(){
+        this.cantidadComprar = 0;
+        this.precioVenta = 0;
+    }
 
-function solicitarCantidad(){
-    let cantidad = prompt("Ingresar cantidad deseada: ");
-    alert("La cantidad que deseas comprar es " + cantidad);
-    return cantidad;
 }
+const producto1 = new Producto("Tabaco Flandria Vainilla x 30gr", "Tabaco", 450, "0.03 kg", "14 x 7 x 2 cm", "Flandria", "Vainilla", 10, 0, 0);
 
-function calcularPrecio(cantidadIngresada, precio){
-    let precioTotal = cantidadIngresada * precio;
-    return precioTotal;
-}
+console.log(producto1);
 
-function mostrarPrecio(precioFinal){
-    alert("El valor final de tu compra es: $" + precioFinal);
-}
-do{
-    cantidadIngresada = solicitarCantidad();
-}while(cantidadIngresada <=0);
+producto1.iva();
+producto1.venta();
+producto1.resetVentaFinalizada();
 
-precioFinal = calcularPrecio(cantidadIngresada, precio);
-mostrarPrecio(precioFinal);
+console.log(producto1);
