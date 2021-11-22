b3b45ccab5a9165056f00a6ec86de16458c97a9b
Carola Cimini <caro.cimini@gmail.com>
2021-11-22 00:53:26 -0300

    Agregando JSON, DOM y Eventos a mi proyecyo

--- js/contacto.js
+++ js/contacto.js
@@ -1,21 +1,41 @@
-const arrayMails = []
+const arrayMails = [];
+const arrayContactos = [];
 
-function obtenerDatos(){
-    var name = document.getElementById("name").value;
-    var mail = document.getElementById("mail").value;
-    var celu = document.getElementById("celu").value;
-    var lista = [name, mail, celu]
-    
-    for (let i=0; i<3; i++){
-        alert("Campo #" + i + ": " + lista[i]);
+let boton = document.getElementById("btnEnviar");
+boton.addEventListener("click", enviarConsulta);
+class Contactos{
+    constructor(id, name, mail, celu){
+        this.id= id;
+        this.name= name;
+        this.mail= mail;
+        this.celu= celu;
     }
-
-    arrayMails.push(mail);
-    console.log ("Cantidad de Mails: " + arrayMails.length);
-    console.log ("Todos los mails: " + arrayMails.join("\n"));
-    
 }
 
 
+function consultarContactos(){
+    console.log ("Cantidad de Contactos: " + arrayContactos.length);
+    console.log ("Todos los Contactos: " + arrayContactos.join("\n"));
+}
 
+function consultarMails(){
+    console.log ("Cantidad de Mails: " + arrayMails.length);
+    console.log ("Todos los mails: " + arrayMails.join("\n"));
+}
 
+function enviarConsulta(){
+
+    let i = arrayContactos.length;
+    var nombre = document.getElementById("name").value;
+    var email = document.getElementById("mail").value;
+    var celular = document.getElementById("celu").value;
+    var comentario = document.getElementById("comentario").value;
+
+    const contacto0 = new Contactos(id= i+1, nombre, email, celular);
+    arrayContactos.push(contacto0);
+    arrayMails.push(email);
+    guardarLocal("listaContactos", JSON.stringify(arrayContactos));
+    alert(nombre + " gracias por tu consutla, te estaremos respondiendo a la brevedad.");
+
+    console.table(arrayContactos);
+}
--- js/product.js
+++ js/product.js
@@ -1,67 +1,116 @@
 class Producto{
-    constructor (id, nombre, rubro, precio, peso, tamaño, marca, sabor, stock, cantidadComprar, precioVenta){
+    constructor (id, nombre, categoria, precio, peso, tamaño, marca, sabor, stock, imagen){
         this.id = id;
         this.nombre = nombre;
-        this.rubro = rubro;
+        this.categoria = categoria;
         this.precio = precio;
         this.peso = peso;
         this.tamaño = tamaño;
         this.marca = marca;
         this.sabor = sabor;
         this.stock = stock;
-        this.cantidadComprar = cantidadComprar;
-        this.precioVenta = precioVenta;
+        this.imagen = imagen;
     }
     
-    iva(){
-        this.precio = this.precio * 1.21;
-    }
-    venta(){
-        this.cantidadComprar = prompt("Ingresar cantidad deseada: ");
-        alert("La cantidad que deseas comprar es " + this.cantidadComprar);
-        this.precioVenta = this.cantidadComprar * this.precio
-        alert("El valor final de tu compra es: $" + this.precioVenta);
-        this.stock = this.stock - this.cantidadComprar;
-    }
-    resetVentaFinalizada(){
-        this.cantidadComprar = 0;
-        this.precioVenta = 0;
+    restaStock(cantidad){
+        this.stock = this.stock - cantidad;
+        console.log(`El stock de ${this.nombre} ha sido actualizado`);
     }
 }
-const producto1 = new Producto(id=1,nombre="Tabaco Flandria Vainilla x 30gr", rubro="Tabaco", precio=450, stock=10, 0, 0);
-const producto2 = new Producto(id=2,nombre="Tabaco Flandria Silver x 30gr", rubro="Tabaco", precio=450, stock=10, 0, 0);
-const producto3 = new Producto(id=3,nombre="Tabaco Flandria ECO x 30gr", rubro="Tabaco", precio=450, stock=10, 0, 0);
-const productos = [producto1, producto2, producto3]
-
-function listarProductos(){
-    for(const producto of productos){
-        console.log("ID: " + producto.id +" "+ producto.nombre);
+class ProductoComprado{
+    constructor(id, nombre, cantidad, precio){
+        this.id = id;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
     }
 }
-function elegirProducto(){
-    var productoElegido = prompt("Ingresa el N° de ID del producto que deseas: ");
-    alert("El producto elegido es: " + productos[productoElegido].nombre);    
+
+
+const producto1 = new Producto(id=1,nombre="Tabaco Flandria Vainilla x 30gr", categoria="Tabaco", 450, peso="30gr", 0, marca="Flandria", sabor="Vainilla", 10, imagen="/blendnroll/imagenes/img-tabacos/Flandria Vainilla x 30gr.png");
+const producto2 = new Producto(id=2,nombre="Tabaco Flandria Silver x 30gr", categoria="Tabaco", 450, peso="30gr", 0, marca="Flandria", sabor="Silver", 10, imagen="/blendnroll/imagenes/img-tabacos/Flandria Silver x 30gr.png");
+const producto3 = new Producto(id=3,nombre="Tabaco Flandria ECO x 30gr", categoria="Tabaco", 450, peso="30gr", 0, marca="Flandria", sabor="Eco", 10, imagen="/blendnroll/imagenes/img-tabacos/Flandria Eco x 30gr.png");
+var arrayProductos = [producto1, producto2, producto3];
+const guardarLocal = (clave, valor) => {localStorage.setItem(clave, valor)};
+guardarLocal("listaProductos", JSON.stringify(arrayProductos));
+
+function imprimirProductos(){
+    let contador = arrayProductos.length;
+    for (let i = 0; i <= contador; i++){
+        let articulo = document.createElement("article");
+        articulo.setAttribute("class", "work container");
+        articulo.innerHTML = `<a href='pageproduct.html'> <img src='${arrayProductos[i].imagen}'>
+        <h3>${arrayProductos[i].nombre}</h3></a>` + `<h4>Precio: $${arrayProductos[i].precio}</h4>` + `<button id='btnComprar${i}' class='btnComprar'>Comprar</button>` 
+        + `<select id='select${i}' class='selectCantidad'>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+    </select>`;
+        var seccion = document.getElementById("seccionProductos");
+        seccion.appendChild(articulo);
+    }
+}  
+console.table(arrayProductos);
+
+let carrito = [];
+
+let productosOfrecidos = "Tenemos para ofrecerte: ";
+
+function agregarCarrito(){
+    for(item of arrayProductos){
+        productosOfrecidos += `\n ${item.id} - ${item.nombre} a $ ${item.precio} la unidad`;
+    }
+
+    productosOfrecidos += `\n Ingrese el número de id del producto que desea agregar al carrito. Para finalizar ingrese 99`;
+    
+    let seleccionUsuario = parseInt(prompt(productosOfrecidos));
+
+    while(isNaN(seleccionUsuario) & seleccionUsuario != 99 || seleccionUsuario > arrayProductos.length & seleccionUsuario != 99 || seleccionUsuario == 0 & seleccionUsuario != 99){
+        alert("Por favor ingrese una opción valida");
+        seleccionUsuario = parseInt(prompt(productosOfrecidos));
+    }
+    
+    while(seleccionUsuario != 99){
+        let cantidad = prompt("Cuantas unidades deseas llevar?");
+        while(cantidad <= 0 || cantidad > arrayProductos[seleccionUsuario -1].stock ){
+            alert("Por favor ingrese una cantidad valida, el stock disponible es de " + arrayProductos[seleccionUsuario-1].stock + " unidades." );
+            cantidad = parseInt(prompt("Cuantas unidades deseas llevar?"));
+        }
+        let precioTotal = cantidad * arrayProductos[seleccionUsuario-1].precio;
+        const comprado = new ProductoComprado(carrito.length+1, arrayProductos[seleccionUsuario-1].nombre, cantidad, precioTotal);
+        carrito.push(comprado);
+        
+        alert(`Agregamos al carrito ${cantidad} unidades de ${arrayProductos[seleccionUsuario-1].nombre}`);
+        arrayProductos[seleccionUsuario-1].restaStock(cantidad);
+
+        seleccionUsuario = parseInt(prompt(productosOfrecidos));
+    }
+    console.log(`Cerramos su pedido!`);
+        mostrarCarrito();
 }
 
-listarProductos();
-elegirProducto();
+let productosCarrito = "Vas a llevar: ";
+let precioCarrito = 0;
+let cantidadProducto = 0;
 
-do{
-    producto1.iva();
-    producto1.venta();
-    producto1.resetVentaFinalizada();
-    console.log(producto1);
-}while(productoElegido == 1)
-do{
-    producto2.iva();
-    producto2.venta();
-    producto2.resetVentaFinalizada();
-    console.log(producto2);
-}while(productoElegido == 2)
-do{
-    producto3.iva();
-    producto3.venta();
-    producto3.resetVentaFinalizada();
-    console.log(producto3);
-}while(productoElegido == 3)
+function mostrarCarrito(){
+    for (itemsElegidos of carrito){
+        productosCarrito += `\n - ${itemsElegidos.nombre} x ${itemsElegidos.cantidad} unidades `;
+        precioCarrito += itemsElegidos.precio;
+    }
+    
+    alert (`Repasemos \n ${productosCarrito} \n por un total de $ ${precioCarrito}`);
+}
+
+agregarCarrito();
+
+imprimirProductos();
+
 
